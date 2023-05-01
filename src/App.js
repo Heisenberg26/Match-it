51ad9dfb702e21606c00cdf8436efd453ca70a1d
ashu260 <devuashutosh26@gmail.com>
2023-05-01 12:00:09 +0530

    Added TinderCards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./Header";
+import Tindercards from "./tindercards";
 function App() {
   return (
     <div className="App">
       {/* <h1>Tinder</h1> */}
       <Header />
+      <Tindercards />
     </div>
   );
 }
--- src/tindercards.js
+++ src/tindercards.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./tindercards.css"
 
-function tindercards() {
+function Tindercards() {
+  const [people, setPeople] = useState(
+    [
+      {
+        name: "Elon Musk",
+        url: "https://m.media-amazon.com/images/M/MV5BMzc1YTA1ZjItMWRhMy00ZTBlLTkzNTgtNTc4ZDE3YTM3ZDk2XkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_FMjpg_UX1000_.jpg"
+      },
+
+    ]);
   return (
     <div className='tinderCards'>
       
@@ -9,4 +17,4 @@ function tindercards() {
   )
 }
 
-export default tindercards
+export default Tindercards
