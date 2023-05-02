fce7dace79ea4d31021bd5a0db6a781e9d876397
ashu260 <devuashutosh26@gmail.com>
2023-05-02 09:59:13 +0530

    Commit - 4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./Header";
 import Tindercards from "./Tindercards";
+import Swipebuttons from "./Swipebuttons";
 function App() {
   return (
     <div className="App">
       {/* <h1>Tinder</h1> */}
       <Header />
       <Tindercards />
+      <Swipebuttons />
     </div>
   );
 }
---
+++ src/swipebuttons.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Swipebuttons() {
+  return (
+    <div className='swipeButtons'>
+      
+    </div>
+  )
+}
+
+export default Swipebuttons
